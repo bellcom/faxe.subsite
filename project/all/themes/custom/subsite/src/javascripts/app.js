73b0ec9fa73c42990c2905a2df025617117c9697
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-16 16:09:22 +0100

    FAXE-114 - header second row

--- project/all/themes/custom/subsite/src/javascripts/app.js
+++ project/all/themes/custom/subsite/src/javascripts/app.js
@@ -30,4 +30,21 @@ jQuery(function($) {
     else {
         $('[data-toggle="tooltip"]').tooltip();
     }
+
+    // Flexy header form
+    function _set_flexy_header_form_position() {
+        let $form = $('.flexy-header__form'),
+            overlap_width = 125, // in pixels (also set in CSS)
+            window_width = $(window).width(),
+            container_width = $('.flexy-header__row--second > .container').outerWidth(true),
+            diff = ((window_width - container_width) / 2);
+
+        $form.css('width', (diff + overlap_width));
+    }
+    _set_flexy_header_form_position();
+
+    // Recalculate width of form when window is resized
+    $(window).on('resize', function(){
+        _set_flexy_header_form_position();
+    });
 });
