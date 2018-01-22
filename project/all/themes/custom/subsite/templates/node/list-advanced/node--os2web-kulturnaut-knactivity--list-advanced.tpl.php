19695b49687e9f0a296189f114b7307a389d2547
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-22 14:27:00 +0100

    FAXE-123, FAXE-115, FAXE-108

---
+++ project/all/themes/custom/subsite/templates/node/list-advanced/node--os2web-kulturnaut-knactivity--list-advanced.tpl.php
@@ -0,0 +1,35 @@
+<?php if ($view_mode == 'list_advanced'): ?>
+  <!-- Begin - list -->
+  <a href="<?php print $node_url; ?>" id="node-<?php print $node->nid; ?>"
+     class="<?php print $classes; ?> element-wrapper-link clearfix"<?php print $attributes; ?>>
+
+    <?php if (isset($content['field_image'])): ?>
+      <!-- Begin - image -->
+      <div class="entity-list-advanced__image">
+        <?php print render($content['field_image']); ?>
+      </div>
+      <!-- End - image -->
+    <?php endif; ?>
+
+    <div class="entity-list-advanced__body">
+
+      <?php if ( isset( $content['field_date'] ) ): ?>
+          <!-- Begin - date -->
+          <div class="entity-list-advanced__date heading-h4">
+              <?php print render( $content['field_date'] ); ?>
+          </div>
+          <!-- End - date -->
+      <?php endif; ?>
+
+      <div class="entity-list-advanced__heading">
+        <h3 class="entity-list-advanced__heading__title heading-h2">
+          <?php print $title; ?>
+        </h3>
+      </div>
+
+      <?php print render($content); ?>
+    </div>
+
+  </a>
+  <!-- End - list -->
+<?php endif; ?>
