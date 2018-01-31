d925680cd7f47c958460acd93cb95687c5c9e7b3
Morten Nissen <mail@hjemmesidekongen.dk>
2018-01-31 15:00:07 +0100

    FAXE-121 - full node

--- project/all/themes/custom/subsite/templates/node/full/node--os2web-base-contentpage--full.tpl.php
+++ project/all/themes/custom/subsite/templates/node/full/node--os2web-base-contentpage--full.tpl.php
@@ -32,6 +32,14 @@
     </div>
     <!-- End - body -->
   <?php endif; ?>
+  
+  <?php if ( isset( $content['field_os2web_base_field_media'] ) ): ?>
+      <!-- Begin - PDF files -->
+      <div class="node__pdf-files">
+          <?php print render( $content['field_os2web_base_field_media'] ); ?>
+      </div>
+      <!-- End - PDF files -->
+  <?php endif; ?>
 
   <?php if (isset($content['field_os2sub_contact_ref'])): ?>
     <!-- Begin - contact block -->
