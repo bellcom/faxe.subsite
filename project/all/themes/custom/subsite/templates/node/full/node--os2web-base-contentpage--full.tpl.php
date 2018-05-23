09cd29bf21789b996f52456d9fdcc86d16c2d3b5
Dmitri Serõn <ds@bellcom.ee>
2018-05-23 10:36:17 +0300

    FAXE-191 Show PDF files on a page style fix.

--- project/all/themes/custom/subsite/templates/node/full/node--os2web-base-contentpage--full.tpl.php
+++ project/all/themes/custom/subsite/templates/node/full/node--os2web-base-contentpage--full.tpl.php
@@ -41,14 +41,6 @@
     <!-- End - contact block -->
   <?php endif; ?>
 
-  <?php if ( isset( $content['field_os2web_base_field_media'] ) ): ?>
-    <!-- Begin - PDF files -->
-    <div class="node__pdf-files">
-      <?php print render( $content['field_os2web_base_field_media'] ); ?>
-    </div>
-    <!-- End - PDF files -->
-  <?php endif; ?>
-
   <?php if (isset($content['field_afsnit'])): ?>
     <!-- Begin - paragraphs -->
     <div class="node__paragraphs">
@@ -64,5 +56,12 @@
     </div>
     <!-- End - webform -->
   <?php endif; ?>
-
+ 
+  <?php if ( isset( $content['field_os2web_base_field_media'] ) ): ?>
+    <!-- Begin - PDF files -->
+    <div class="node__pdf-files">
+      <?php print render( $content['field_os2web_base_field_media'] ); ?>
+    </div>
+    <!-- End - PDF files -->
+  <?php endif; ?>
 </article>
