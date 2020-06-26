e1ae877409e169897b5439bc3f91e9043ebfc628
Morten Nissen <mail@hjemmesidekongen.dk>
2020-06-26 10:08:22 +0200

    Theme

--- project/all/themes/custom/subsite/templates/node/full/node--os2web-base-contentpage--full.tpl.php
+++ project/all/themes/custom/subsite/templates/node/full/node--os2web-base-contentpage--full.tpl.php
@@ -9,14 +9,6 @@
     <!-- End - image -->
   <?php endif; ?>
 
-  <?php if (!empty($title)): ?>
-    <!-- Begin - heading -->
-    <div class="node__heading">
-      <h1 class="node__heading__title"><?php print $title; ?></h1>
-    </div>
-    <!-- End - heading -->
-  <?php endif; ?>
-
   <?php if (isset($content['field_os2web_base_field_summary'])): ?>
     <!-- Begin - intro -->
     <div class="node__intro">
